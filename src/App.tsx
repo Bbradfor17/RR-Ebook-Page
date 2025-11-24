82095bac24313a707cd1d9c6df8fc2e03746d539
Ben Byrd <bbyrd@inmarket.com>
2025-11-23 21:05:15 -0500

    Inline styles, cleanup code, switch to pnpm, introduce linter, update core deps, edit copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './theme';
-import Hero from './components/Hero';
-import WhatsInside from './components/WhatsInside';
-import LeadForm from './components/LeadForm';
-import Footer from './components/Footer';
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { theme } from "./theme";
+import { Hero, WhatsInside, LeadForm, Footer } from "./components";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Hero />
-      <WhatsInside />
-      <LeadForm />
-      <Footer />
+      <Box
+        sx={{
+          maxWidth: "1280px",
+          mx: "auto",
+          px: { xs: 2, md: 4 },
+          py: { xs: 3, md: 5 },
+          textAlign: "center",
+        }}
+      >
+        <Hero />
+        <WhatsInside />
+        <LeadForm />
+        <Footer />
+      </Box>
     </ThemeProvider>
   );
 }
